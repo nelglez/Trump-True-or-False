56bb12f9f2af02b7ccf5f93297d512cbc2d9ec9c
Nelson Gonzalez <nelglezfl@gmail.com>
2020-07-11 10:55:54 -0400

    Initial Commit

---
+++ Trump True or False/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Trump True or False
+//
+//  Created by Nelson Gonzalez on 7/11/20.
+//  Copyright © 2020 Nelson Gonzalez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
